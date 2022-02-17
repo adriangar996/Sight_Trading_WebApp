bbb0d70cb3f111a35ec3b152e9101f93f71bf3f1
Cesar Borri <cesar.borri4@gmail.com>
2022-02-17 13:07:18 -0400

    Update predictions.py
    Self updating version

--- predictions.py
+++ predictions.py
@@ -1,16 +1,16 @@
 #Machine Learning Algorithm
 import yfinance as yf
-import math # For estimation
+#import math # For estimation
 import numpy as np # Processing data as arrays
 import pandas as pd # Used to create a dataframe that holds all data
 from sklearn.preprocessing import MinMaxScaler # Used to scale data
-import tensorflow as tf # Importation of library used for model creation
-from tensorflow import keras # Importation of backend of tensorflow
-from keras.models import Sequential # Importation of a sequentional model form
-from keras.layers import Dense, LSTM # Importation of Neural Network layers and LSTM layers
+#import tensorflow as tf # Importation of library used for model creation
+#from tensorflow import keras # Importation of backend of tensorflow
+#from keras.models import Sequential # Importation of a sequentional model form
+#from keras.layers import Dense, LSTM # Importation of Neural Network layers and LSTM layers
 from keras.models import load_model # Used to load existing models
 import datetime
-
+import joblib
 def models_loader(folder, name, days = [1, 5, 30]):
     model = []
     for i in days:
@@ -18,29 +18,39 @@ def models_loader(folder, name, days = [1, 5, 30]):
     return model
 
 days = [1,5,14,30,90] #Los Dias que el modelo funciona
-scaler = MinMaxScaler(feature_range=(0,1))
+scaler = joblib.load('scaler.sav')
 
-models = models_loader('Model_T','ModelT', days) #lodear los modelos 
+models = models_loader('ML Model','Model', days) #lodear los modelos 
 #Buscar el dia y calcular 75 dias en el pasado
-last = datetime.date.today() 
-td = datetime.timedelta(100)
-start = last - td
+#last = datetime.date.today() 
+#td = datetime.timedelta(100)
+#start = last - td
 
 companies = ['TSLA', 'AAPL','SIRI','GGB','PLUG']
 #print(f'Start: {start}, Last: {last}')
 
-
-for symbols in companies:
-    print(symbols)
-    predINV= []
-    data = yf.download(symbols,start = start, end = last)
-    data = data.filter(['Close'])
-    INPUT = data.values
-    INPUT = INPUT[-60:]
-    scaled_input = scaler.fit_transform(INPUT)
-    scaled_input = np.reshape(scaled_input, (-1,60,1))
-    for i in range(len(days)):
-        pred = models[i].predict(scaled_input)
-        predINV.append(scaler.inverse_transform(pred))
-        predINV[i] = predINV[i].round(decimals = 2)
-        print(f'Day {days[i]}: {predINV[i]}')
+while True:
+	time = datetime.datetime.today()
+	schedule = datetime.time(17,0,0)
+	if time.hour == schedule.hour:
+		last = datetime.date.today() 
+		td = datetime.timedelta(100)
+		start = last - td
+		SData = pd.read_csv("http://www.nasdaqtrader.com/dynamic/SymDir/nasdaqtraded.txt", sep='|')
+		SData = SData[SData['Test Issue'] == 'N']	
+		companies = SData[SData['ETF']=='N']['NASDAQ Symbol'].tolist()
+		
+		for symbols in companies:
+			print(symbols)
+			predINV= []
+			data = yf.download(symbols,start = start, end = last)
+			data = data.filter(['Close'])
+			INPUT = data.values
+			INPUT = INPUT[-60:]
+			scaled_input = scaler.fit_transform(INPUT)
+			scaled_input = np.reshape(scaled_input, (-1,60,1))
+			for i in range(len(days)):
+				pred = models[i].predict(scaled_input) 
+				predINV.append(scaler.inverse_transform(pred))
+				predINV[i] = predINV[i].round(decimals = 2)
+				print(f'Day {days[i]}: {predINV[i]}')
