061bbb8fda37dc4b0f57a3b6182078fed219a98a
Adrian Garcia <71535957+adriangar996@users.noreply.github.com>
2022-02-23 01:57:56 -0400

    Commit

--- Dashboards/views.py
+++ Dashboards/views.py
@@ -3,10 +3,57 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from Portfolios.models import PortfolioUser
+from yahoo_finance import Share
+import requests
+import json
 
 
 
 @login_required
 def indexView(request):
 
-    return render(request, 'dashboardindex.html')  
+    url = "https://yfapi.net/v6/finance/quote"
+
+    querystring = {"symbols":"NDAQ"}
+
+    headers = {
+    'x-api-key': "iFc6RqsSZ31mlsJY7frhf3RkQbjyn4325Dztkxy2"
+    }
+
+    r = requests.request("GET", url, headers=headers, params=querystring)
+    response = r.json()
+    
+    prev_close = response['regularMarketPreviousClose']
+    day_range = response['regularMarketDayRange']
+    year_range = response['fiftyTwoWeekRange']
+
+    context={
+      'prev_close' : prev_close,  
+      'day_range' : day_range,
+      'year_range' : year_range,
+    }
+
+    return render(request, 'dashboardindex.html', context)  
+
+
+
+
+
+
+
+
+#   index = Share('NDAQ')
+
+#     prev_close = index.get_prev_close()
+#     day_high = index.get_days_high()
+#     day_low = index.get_days_low()
+#     year_high = index.get_year_high()
+#     year_low = index.get_year_low()
+
+#     context={
+#         'prev_close' : prev_close,
+#         'day_high' : day_high,
+#         'day_low' : day_low,
+#         'year_high' : year_high,
+#         'year_low' : year_low,
+#     }
