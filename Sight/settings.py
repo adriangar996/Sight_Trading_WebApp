beba922f44966831c9b9ea39f84bb9c0e6210414
Adrian Garcia <71535957+adriangar996@users.noreply.github.com>
2022-03-20 19:58:41 -0400

    Commit

--- Sight/settings.py
+++ Sight/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
