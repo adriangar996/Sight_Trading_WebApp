703da4472a23158c18c47364e74479b3da50fc20
Adrian Garcia <71535957+adriangar996@users.noreply.github.com>
2022-03-20 21:06:40 -0400

    Test

--- Landing/views.py
+++ Landing/views.py
@@ -7,7 +7,7 @@ from django.db.models.query_utils import Q
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
-from django.views.decorators.csrf import csrf_exempt
+#from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.forms import PasswordResetForm
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -20,7 +20,7 @@ def homeView(request):
 
     return render(request, 'landing.html')   
 
-@csrf_exempt
+
 def loginView(request):
   '''Form support for Login'''
   if request.method == "POST":
