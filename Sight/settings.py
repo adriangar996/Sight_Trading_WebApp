6a1965474e89ee76d44adf4d57d3a07ece25dfa0
Adrian Garcia <71535957+adriangar996@users.noreply.github.com>
2022-03-20 21:37:47 -0400

    test

--- Sight/settings.py
+++ Sight/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9kwujr&k898n(s-lc9v%6pw%syg%4k#u*6$2k(-nh)qf=b6c4g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sightstockapp.azurewebsites.net']
+#ALLOWED_HOSTS = []
 
 
 MESSAGE_TAGS = {
@@ -65,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    #'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Sight.urls'
